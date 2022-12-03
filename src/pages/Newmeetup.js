f1b436f114cd3b0fab4b03883262bc2f8c32a8a7
sammy-yuntin <yuntinsammy@yahoo.com>
2022-12-03 18:43:18 +0100

    On successful data submit, an alert has been infused to notify user and a route to the index/homepage

--- src/pages/Newmeetup.js
+++ src/pages/Newmeetup.js
@@ -1,7 +1,10 @@
 import React from "react";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import {useNavigate} from "react-router-dom"
 
 function NewMeetup(){
+    
+    const history = useNavigate();
 
     function addMeetupHandler(meetupData){
         fetch("https://second-attempt-for-meetupapp-default-rtdb.firebaseio.com/meetups.json", 
@@ -12,7 +15,10 @@ function NewMeetup(){
                     'Content-Type':'application/json'
                 }
             }
-        );
+        ).then( function thenHandler() {
+            history("/");
+            alert("Data Submitted")
+        });
     }
 
     return(
